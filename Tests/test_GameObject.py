20e581afea86603a5c62488bffffbc4ac67466b0
Fermin Verdolini <ferminverdolini@gmail.com>
2022-05-31 17:00:13 -0300

    Test Actualizar

--- Tests/test_GameObject.py
+++ Tests/test_GameObject.py
@@ -1,24 +1,43 @@
+import pygame
 from gameObject import gameObject
 
 
 class test_gameObject:
 
     def testMoverPositivo():
-        obj = gameObject()
+        obj = gameObject(pygame.image.load("Sprites/bird.png"))
         obj.mover(300, 250)
         assert obj.getPosicion == (300, 250)
         
     def testMoverNegativo():
-        obj = gameObject()
+        obj = gameObject(pygame.image.load("Sprites/bird.png"))
         obj.mover(-300, -250)
         assert obj.getPosicion == (-300, -250)
 
     def testMoverCero():
-        obj = gameObject()
+        obj = gameObject(pygame.image.load("Sprites/bird.png"))
         obj.mover(0, 0)
         assert obj.getPosicion == (0, 0)
     
     def testMoverAfuera():
-        obj = gameObject()
+        obj = gameObject(pygame.image.load("Sprites/bird.png"))
         obj.mover(-1000, 1000)
         assert obj.getPosicion == (-300, -250)
+
+    def testActualizar60fps():
+        obj = gameObject(pygame.image.load("Sprites/bird.png"))
+        obj.setVelocidad(5,7)
+        obj.actualizar(0.016)
+        assert obj.getPosicion() == (5*0.016, 7*0.016)
+
+    def testActualizar20fps():
+        obj = gameObject(pygame.image.load("Sprites/bird.png"))
+        obj.setVelocidad(5,7)
+        obj.actualizar(0.05)
+        assert obj.getPosicion() == (5*0.05, 7*0.05)
+
+    def testActualizar100fps():
+        obj = gameObject(pygame.image.load("Sprites/bird.png"))
+        obj.setVelocidad(5,7)
+        obj.actualizar(0.01)
+        assert obj.getPosicion() == (5*0.01, 7*0.01)
