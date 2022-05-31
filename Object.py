847ae3937a1a8dc60766b65d6f924b17ab1235d2
Fermin Verdolini <ferminverdolini@gmail.com>
2022-05-31 01:37:35 -0300

    tabs

--- Object.py
+++ Object.py
@@ -1,15 +1,15 @@
 class Object:
-    
+
     _posicionX = 0
     _posicionY = 0
     _velocidadX = 0
     _velocidadY = 0
-    
+
     _sprite = None
-    
+
     def __init__(self, sprite):
         self._sprite = sprite
-    
+
     def mover(self, x, y):
         self._posicionX = x
         self._posicionY = y
