90e92b6c21236036ef40d9bcb5f57f55a5252790
Fermin Verdolini <ferminverdolini@gmail.com>
2022-05-31 11:25:03 -0300

    Creacion clase vista,render y creacion de ventana

---
+++ vista.py
@@ -0,0 +1,13 @@
+from curses import window
+import pygame
+
+
+class vista:
+    __anchoVentana =600
+    __altoVentana =500
+    
+    ventana = pygame.display.set_mode((__anchoVentana,__altoVentana))
+
+    def crearVentana(self):
+        window.blit(pygame.image.load("Sprites/background.png"), (0, 0))
+        window.blit(pygame.image.load("Sprites/bird.png"), (120, 250))
