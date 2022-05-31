fd83bedf0b9c1a63535e29369742e9d5bcbe7855
SrNandu <octaula@gmail.com>
2022-05-31 01:52:02 -0300

    Clase Game

--- game.py
+++ game.py
@@ -1,1 +1,30 @@
-print("Hello World")
+from ast import Import
+from Object import Object 
+import pygame
+
+class Game:
+
+    __pajaro = Object(pygame.image.load("Sprites/bird.png"))
+    __tuberiasArriba = []
+    __tuberiasAbajo = []
+    __input = None
+
+    __puntaje = 0
+
+    def __init__(self,input):
+        self.__input = input
+        
+    def Loop(self):
+        while True:
+
+            if(self.Choco(self.__tuberiasArriba,self.__tuberiasAbajo,self.__pajaro)):
+                pass
+
+            if(self.Gol(self.__tuberiasArriba,self.__tuberiasAbajo,self.__pajaro)):
+                self.__puntaje += 1
+            
+    def Choco(tuberiasArriba,tuberiasAbajo,pajaro):
+        return False
+
+    def Gol(tuberiasArriba,tuberiasAbajo,pajaro):
+        return False
