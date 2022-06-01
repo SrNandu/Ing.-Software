924522be9f8c28d441a97ef9d120ede0123f4e70
SrNandu <octaula@gmail.com>
2022-06-01 01:55:00 -0300

    Randomizador tuberias

--- Source/tuberia.py
+++ Source/tuberia.py
@@ -1,20 +1,22 @@
+import random
 from pygame import Surface
 from gameObject import gameObject
 
 
 class tuberia(gameObject):
 
-    def __init__(self, sprite : Surface):
+    def __init__(self, sprite: Surface):
         self._velocidadX = -30
         super().__init__(sprite)
 
     def posicionar(self, anchoVentana: int, altoVentana: int, i: int, arriba: bool):
-        espacio = 30
+        espacio = 50
+        rand = random.randint(-50, 50)
 
         alto = self._sprite.get_size()[1]
 
         if arriba:
-            self.mover(anchoVentana / 2 + 100 * i, altoVentana / 2 + espacio)
+            self.mover(anchoVentana / 2 + 150 * i, altoVentana / 2 + espacio + rand)
         else:
-            self.mover(anchoVentana / 2 + 100 * i,
-                       altoVentana / 2 - espacio - alto)
+            self.mover(anchoVentana / 2 + 150 * i,
+                       altoVentana / 2 - espacio - alto + rand)
