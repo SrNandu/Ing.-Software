408c03e453b14489a583267818031c174dcda419
SrNandu <octaula@gmail.com>
2022-06-01 00:40:13 -0300

    Tests

--- Source/Tests/test_GameObject.py
+++ Source/Tests/test_GameObject.py
@@ -14,27 +14,27 @@ sys.path.append(padre)
 from gameObject import gameObject
 
 
-class test_gameObject:
+class TestGameObject:
 
-    def testMoverPositivo(self):
+    def test_mover_positivo(self):
         obj = gameObject(pygame.image.load("Sprites/bird.png"))
         obj.mover(300, 250)
-        assert obj.getPosicion == (300, 250)
+        assert obj.getPosicion() == (300, 250)
 
     def testMoverNegativo(self):
         obj = gameObject(pygame.image.load("Sprites/bird.png"))
         obj.mover(-300, -250)
-        assert obj.getPosicion == (-300, -250)
+        assert obj.getPosicion() == (-300, -250)
 
     def testMoverCero(self):
         obj = gameObject(pygame.image.load("Sprites/bird.png"))
         obj.mover(0, 0)
-        assert obj.getPosicion == (0, 0)
+        assert obj.getPosicion() == (0, 0)
 
     def testMoverAfuera(self):
         obj = gameObject(pygame.image.load("Sprites/bird.png"))
         obj.mover(-1000, 1000)
-        assert obj.getPosicion == (-300, -250)
+        assert obj.getPosicion() == (-1000, 1000)
 
     def testActualizar60fps(self):
         obj = gameObject(pygame.image.load("Sprites/bird.png"))
