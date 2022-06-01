0130ffa7ac7127807a00ba83d598f56173609d01
SrNandu <octaula@gmail.com>
2022-05-31 21:23:36 -0300

    Ancho y Alto en constructor de vista

--- vista.py
+++ vista.py
@@ -3,16 +3,26 @@ import pygame
 from gameObject import gameObject
 
 class vista:
-    ANCHO_VENTANA = 600
-    ALTO_VENTANA = 500
-    
-    __ventana = pygame.display.set_mode((ANCHO_VENTANA,ALTO_VENTANA))
+    __ancho = 0
+    __alto = 0   
+    __ventana = None
+
+    def __init__(self, ancho: int, alto : int):
+        self.__alto = alto
+        self.__ancho = ancho
+        self.__ventana = pygame.display.set_mode(self.__ancho, self.__alto)
 
     def renderizar(self, gameObject: gameObject):
         self.__ventana.blit(gameObject.getSprite(), gameObject.getPosicion())
 
     def actualizarVentana(self):
         pygame.display.update()
+
+    def getAncho(self):
+        return self.__ancho
+
+    def getAlto(self):
+        return self.__alto
 
 
         
