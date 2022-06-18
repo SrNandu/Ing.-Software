bef0295593c70f8b22990a54dc90ee440cb46f3b
Octavio Ulla <octaula@gmail.com>
2022-06-18 02:43:39 -0300

    Fixture tests

--- Source/Tests/test_game.py
+++ Source/Tests/test_game.py
@@ -1,38 +1,44 @@
+import pytest
 import sys
 import os
 
-#Nombre del directorio
+# Nombre del directorio
 actual = os.path.dirname(os.path.realpath(__file__))
-  
-#Nombre del directorio padre
+
+# Nombre del directorio padre
 padre = os.path.dirname(actual)
-  
-#Añadir el directorio padre al sys.path
+
+# Añadir el directorio padre al sys.path
 sys.path.append(padre)
 
-from Model.GameObject import GameObject
-import pygame
+from Model.Game import Game
 
-class TestGame:
+@pytest.fixture
+def game500x600():
+    return Game(500, 600)
 
-    def test_initTuberias_500x600(self):
-        assert True
+@pytest.fixture
+def game1000x12000():
+    return Game(1000, 12000)
 
-    def test_initTuberias_1000x1200(self):
-        assert True
-    
-    def test_initPajaro_500x600(self):
-        assert True
-    
-    def test_initPajaro_1000x1200(self):
-        assert True
-    
-    def test_añadirTuberias(self):
-        assert True
+def test_initTuberias_500x600(game500x600):
+    assert True
 
-    def test_moverPajaro_y500(self):
-        assert True
+def test_initTuberias_1000x1200(game1000x12000):
+    assert True
 
-    def test_moverPajaro_y200(self):
-        assert True
+def test_initPajaro_500x600(game500x600):
+    assert True
 
+def test_initPajaro_1000x1200(game1000x12000):
+    assert True
+
+def test_añadirTuberias(game500x600):
+    assert True
+
+def test_moverPajaro_y500(game500x600):
+    assert True
+
+
+def test_moverPajaro_y200(game500x600):
+    assert True
