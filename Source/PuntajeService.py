ce00fe7004f2f758a0a5b98f40cb4b32cf26f764
FerminVerdolini <ferminverdolini@gmail.com>
2022-06-18 16:42:15 -0300

    set and get input strategy

--- Source/PuntajeService.py
+++ Source/PuntajeService.py
@@ -1,9 +1,9 @@
 import os
-class puntajeService: 
+class PuntajeService: 
     
     __instance: None
     __puntajeMax: int
-
+    __inputStrategy: None #Class ImputStrategy
     def __init__(self):
 
         if os.path.isfile('Textos/puntaje.txt'):
@@ -13,3 +13,11 @@ class puntajeService:
         else: 
             file = open("Textos/puntaje.txt", "w")            
             file.write("Puntaje maximo:")
+
+    def getImputStrategy(self):
+        return self.__inputStrategy
+
+    def setImputStrategy(self, inputStrategy):
+       self.__inputStrategy = inputStrategy
+
+           
