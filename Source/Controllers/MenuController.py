6b9eeabcc42d11a43a38d4408c075ab1c6cedc06
Octavio Ulla <octaula@gmail.com>
2022-06-19 16:16:55 -0300

    Menu and Menu Controller

---
+++ Source/Controllers/MenuController.py
@@ -0,0 +1,13 @@
+from ast import Sub
+from Model.Game import Game
+from Controllers.Controller import Controller
+from Model.Menu import Menu
+from Subject import Subject
+from Window import Window
+
+
+class GameController(Controller):
+    __input = None
+
+    def __init__(self, menu: Menu):
+        super().__init__(menu)
---
+++ Source/Model/Menu.py
@@ -0,0 +1,26 @@
+from Subject import Subject
+
+
+class Menu(Subject):
+    __boton: int = 0
+    __cantBotones: int
+
+    def __init__(self, cantBotones):
+        self.__cantBotones == cantBotones
+
+    def botonSig(self):
+        self.__boton = self.__boton + 1
+        if self.__boton == self.__cantBotones:
+            self.__boton = 0
+
+        self._notify(self)
+
+    def botonAnt(self):
+        self.__boton = self.__boton - 1
+        if self.__boton == -1:
+            self.__boton = self.__cantBotones
+        
+        self._notify(self)
+
+    def getBoton(self):
+        return self.__boton
