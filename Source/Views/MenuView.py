e00f6cc9135e9a2e168431731de61eb5bb922e4a
Octavio Ulla <octaula@gmail.com>
2022-06-19 17:06:09 -0300

    Puntaje max en Menu

--- Source/Views/MenuView.py
+++ Source/Views/MenuView.py
@@ -1,12 +1,18 @@
+from importlib.metadata import SelectableGroups
+
+import PyQt5
 from Model.Menu import Menu
+from PuntajeService import PuntajeService
 from Views.View import View
 from Controllers.Controller import Controller
-from PyQt5.QtWidgets import QGridLayout, QPushButton
+from PyQt5.QtWidgets import QGridLayout, QPushButton, QLabel, QSizePolicy
+from PyQt5.QtCore import *
 
 
 class MenuView(View):
-
     __botones: list[QPushButton] = []
+    __puntajeText: QLabel
+    __puintajeService: PuntajeService = PuntajeService()
 
     def __init__(self, controller: Controller):
         super().__init__(controller)
@@ -15,6 +21,10 @@ class MenuView(View):
         self.__botones.append(QPushButton("InputCabeza"))
         self.__botones.append(QPushButton("InputMano"))
 
+        puntaje = str(self.__puintajeService.getPuntajeMax())
+        self.__puntajeText = QLabel("Puntaje Máximo: " + puntaje)
+        self.__puntajeText.setAlignment(Qt.AlignHCenter)
+
         main_layout = QGridLayout(self)
         main_layout.setHorizontalSpacing(15)
         main_layout.setVerticalSpacing(15)
@@ -22,6 +32,7 @@ class MenuView(View):
         main_layout.addWidget(self.__botones[0], 0, 0, 1, 2)
         main_layout.addWidget(self.__botones[1], 1, 1, 1, 1)
         main_layout.addWidget(self.__botones[2], 1, 0, 1, 1)
+        main_layout.addWidget(self.__puntajeText, 2, 0, 1, 2)
 
         self.setLayout(main_layout)
         self.setVisible(True)
