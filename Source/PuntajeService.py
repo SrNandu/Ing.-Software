a0e074a9eef42b350539938fcb5b7ef1903ada2f
FerminVerdolini <ferminverdolini@gmail.com>
2022-06-18 21:04:57 -0300

    arreglos en puntaje service

--- Source/PuntajeService.py
+++ Source/PuntajeService.py
@@ -1,32 +1,38 @@
+from io import TextIOWrapper
 import os
 class PuntajeService: 
     
-    __instance: None
-    __puntajeMax: int
+    __puntajeMax: int = 0
     __inputStrategy: None #Class ImputStrategy
     def __init__(self):
-
+        file: TextIOWrapper
         if os.path.isfile('Textos/puntaje.txt'):
-            with open("Textos/puntaje.txt","r") as archivo:
-                for linea in archivo:
-                    print(linea)
+            
+            with open("Textos/puntaje.txt","r") as file:
+                
+               __puntajeMax = int(file.read()) 
+                
         else: 
             file = open("Textos/puntaje.txt", "w")            
-            file.write("Puntaje maximo:")
+            file.write("0")
+            __puntajeMax = int(file.read)
+
+
 
     def getImputStrategy(self):
         return self.__inputStrategy
 
-    def setImputStrategy(self, inputStrategy):
+    def setImputStrategy(self, inputStrategy ):#:ImputStategy
        self.__inputStrategy = inputStrategy
 
     def getPuntajeMax(self):
         return self.__puntajeMax
 
-    def setPuntajeIfMax(self, puntaje):
+    def setPuntajeIfMax(self, puntaje :int):
         if(puntaje> self.__puntajeMax):
-            self.__setPuntajeMax()  
+            self.__setPuntajeMax(puntaje)  
 
-    def __setPuntajeMax(self):
-            file = open("Textos/puntaje.txt", "w")            
+    def __setPuntajeMax(self, puntaje :int):
+        self.__puntajeMax = puntaje        
+        with open("Textos/puntaje.txt", "w") as file: 
             file.write("Puntaje maximo:" + self.__puntajeMax)
